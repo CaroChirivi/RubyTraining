b86aa1d81935119a85a4a53459bdfdf0945e40ef
CaroChirivi <carotarapues@gmail.com>
2021-09-08 12:00:50 -0500

    Create CRUD for books

---
+++ apistore/db/migrate/20210908170007_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.float, :price
+      t.string, :category
+      t.string, :author
+      t.string, :editor
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
