fff58315974322707c924433084d42ba620212e1
CaroChirivi <carotarapues@gmail.com>
2021-09-08 12:16:29 -0500

    Create migrations and validations for books

--- apistore/db/migrate/20210908170007_create_books.rb
+++ apistore/db/migrate/20210908170007_create_books.rb
@@ -2,10 +2,10 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :name
-      t.float, :price
-      t.string, :category
-      t.string, :author
-      t.string, :editor
+      t.float :price
+      t.string :category
+      t.string :author
+      t.string :editor
       t.string :description
 
       t.timestamps
--- apistore/db/schema.rb
+++ apistore/db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_164257) do
+ActiveRecord::Schema.define(version: 2021_09_08_170007) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.string "category"
+    t.string "author"
+    t.string "editor"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
