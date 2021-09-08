d9e09e233dcf431c841439dc8d50895340d32af3
CaroChirivi <carotarapues@gmail.com>
2021-09-08 15:39:38 -0500

    Add category validation for books, and platform validation for games (values to include) and only positive numbers for price in both

--- apistore/app/models/book.rb
+++ apistore/app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ApplicationRecord
     validates :name, :price, :author, presence: true
+    validates :category, inclusion: { in: %w(adventure romance scifi horror),
+        message: "%{value} is not a valid platform" }
     validates :price, numericality: true
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
     validates :author, length: { in: 3..30 }
     validates :description, length: { maximum: 250 }
 end
--- apistore/app/models/game.rb
+++ apistore/app/models/game.rb
@@ -1,5 +1,8 @@
 class Game < ApplicationRecord
     validates :name, :price, presence: true
+    validates :platform, inclusion: { in: %w(PS4 XBox PC),
+        message: "%{value} is not a valid platform" }
     validates :price, numericality: true
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
     validates :description, length: { maximum: 250 }
 end
