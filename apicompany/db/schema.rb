653a75ba3b507713143b1ba212176647fad04ad6
CaroChirivi <carotarapues@gmail.com>
2021-09-10 11:10:21 -0500

    Creating CRUD for countries

--- apicompany/db/schema.rb
+++ apicompany/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_10_155836) do
+ActiveRecord::Schema.define(version: 2021_09_10_160849) do
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
