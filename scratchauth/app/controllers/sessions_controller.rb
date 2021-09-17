7f4cefcdecede0f401182962616d7aac11b9dc3a
CaroChirivi <carotarapues@gmail.com>
2021-09-17 17:59:28 -0500

    Creating funcionality for signin and login creating view for login

--- scratchauth/app/controllers/sessions_controller.rb
+++ scratchauth/app/controllers/sessions_controller.rb
@@ -4,6 +4,19 @@ class SessionsController < ApplicationController
     end
 
     def create
-        render params
+        email = params[:email]
+        password = params[:password]
+        user = User.find_by(:email => email) 
+        if user && user.password_digest == password
+            response = {
+                :loggedin => true
+            }
+            render json: response
+        else
+            response = {
+                :loggedin => false
+            }
+            render 'new'
+        end
     end
 end
--- scratchauth/app/controllers/users_controller.rb
+++ scratchauth/app/controllers/users_controller.rb
@@ -6,17 +6,16 @@ class UsersController < ApplicationController
     def create
         email = params[:username]
         password = params[:userpassword]
-        User.create(
-            :email => email,
-            :password_digest => password
-        )
-        @success = true
-        @message = 'Your account is created successfully'
-        # response = {
-        #     :success => true
-        # } 
-        # render json: response
-
+        user = User.find_by(:email => email)
+        if user
+            @register = false
+        else
+            User.create(
+                :email => email,
+                :password_digest => password
+            )
+            @register = true
+        end
         render 'new'
     end
 
